30c94ec8e102a88fff2738a76805b0dfd633130d
Izabela Cecilia <icsbarbosa@sga.pucminas.br>
2024-06-06 22:05:40 -0300

    ajustando coluna

--- projeto-01/front-end/todo-list/src/App.js
+++ projeto-01/front-end/todo-list/src/App.js
@@ -69,8 +69,7 @@ function App() {
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
                         <TableHead>
                             <TableRow>
-                                <TableCell>Dessert (100g serving)</TableCell>
-                                <TableCell align="right">Calories</TableCell>
+                                <TableCell align="right">Titulo</TableCell>
                                 <TableCell align="right">Fat&nbsp;(g)</TableCell>
                                 <TableCell align="right">Carbs&nbsp;(g)</TableCell>
                                 <TableCell align="right">Protein&nbsp;(g)</TableCell>
@@ -82,10 +81,7 @@ function App() {
                                     key={row.name}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                 >
-                                    <TableCell component="th" scope="row">
-                                        {row.name}
-                                    </TableCell>
-                                    <TableCell align="right">{row.calories}</TableCell>
+                                    <TableCell align="right">{row.titulo}</TableCell>
                                     <TableCell align="right">{row.fat}</TableCell>
                                     <TableCell align="right">{row.carbs}</TableCell>
                                     <TableCell align="right">{row.protein}</TableCell>
