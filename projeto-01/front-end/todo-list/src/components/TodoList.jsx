f1fc9c6b15ece4ec7116ca4dc4d0089f5380230a
marcotulio21mm <marcotulio21mm@gmail.com>
2024-06-15 20:09:33 -0300

    ajustes no layout

--- projeto-01/front-end/todo-list/src/components/TodoList.jsx
+++ projeto-01/front-end/todo-list/src/components/TodoList.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Checkbox, IconButton } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableRow, Paper, Checkbox, IconButton } from '@mui/material';
 import { Icon } from '@iconify/react';
 import dayjs from 'dayjs';
 
 const TodoList = ({ tasks, onCheckboxChange, onDelete }) => {
-  const calculateDeliveryDate = (task) => {
+  const calculateDeliveryMessage = (task) => {
     const today = dayjs();
     if (task.tarefa === 'PRAZO') {
       const dueDate = dayjs().add(task.diasPrevisto, 'day');
       if (dueDate.isBefore(today, 'day')) {
-        return 'Data de conclusão vencida';
+        return 'Prazo para conclusão vencido';
       } else {
         return `Prazo para conclusão: ${task.diasPrevisto} dias`;
       }
@@ -21,28 +21,20 @@ const TodoList = ({ tasks, onCheckboxChange, onDelete }) => {
         return `Data para conclusão: ${expectedDate.format('DD/MM/YYYY')}`;
       }
     } else {
-      return 'N/A';
+      return ''; // Retorna uma string vazia para outros tipos de tarefa
     }
   };
 
   return (
     <TableContainer component={Paper} sx={{ maxWidth: "80%", marginLeft: "auto", marginRight: "auto", marginTop: "2rem" }}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left"></TableCell>
-            <TableCell align="left">Título</TableCell>
-            <TableCell align="left">Prioridade</TableCell>
-            <TableCell align="left">Status</TableCell>
-            <TableCell align="left">Tipo</TableCell>
-            <TableCell align="left">Data de Entrega</TableCell>
-            <TableCell align="left">Ações</TableCell>
-          </TableRow>
-        </TableHead>
         <TableBody>
           {tasks.map((task) => (
             <TableRow key={task.id}>
-              <TableCell align="left">
+              <TableCell align="left" sx={{ padding: '8px', verticalAlign: 'top', width: "30%" }}>
+                {(task.status || calculateDeliveryMessage(task) !== '') && ( // Mostra a mensagem se a tarefa estiver concluída ou se houver mensagem de prazo/data
+                  <div style={{ marginBottom: '8px' }}>{calculateDeliveryMessage(task)}</div>
+                )}
                 <Checkbox
                   checked={task.status === true}
                   onChange={() => onCheckboxChange(task)}
@@ -53,11 +45,9 @@ const TodoList = ({ tasks, onCheckboxChange, onDelete }) => {
                   }}
                 />
               </TableCell>
-              <TableCell align="left">{task.titulo}</TableCell>
-              <TableCell align="left">{task.prioridade}</TableCell>
-              <TableCell align="left">{task.status ? 'Concluída' : 'Pendente'}</TableCell>
-              <TableCell align="left">{task.tarefa}</TableCell>
-              <TableCell align="left">{calculateDeliveryDate(task)}</TableCell>
+              <TableCell align="left">
+                <div>{task.titulo}</div>
+              </TableCell>
               <TableCell align="left">
                 <IconButton onClick={() => onDelete(task)}>
                   <Icon icon={"mdi:trash"} />
